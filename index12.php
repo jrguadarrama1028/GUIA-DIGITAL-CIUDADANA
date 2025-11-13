72645b781ee22582e77dc0cbe4bd7988b0c3be52
jr_guadarrama1028 <mguadarrama442@gmail.com>
2025-11-12 18:54:13 -0600

    Add files via upload

--- index12.php
+++ index12.php
@@ -2,13 +2,12 @@
 <html>
 <head>
     <title>ASISTENCIA GRUPO 502</title>
-   <meta charset="UTF-8">
+    <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="estilos_jr.css">
 </head>
 <body>
-<img src="cecytem-logo.png"align="left"width="100" height="100">
+    <img src="logo.png"align="left"width="100" height="100">
 <img src="logo23.jpeg"align="right"width="100" height="100">
-    
     
     <font face="calibri" size=15 color=green>COLEGIO DE ESTUDIOS CIENT&Iacute;FICOS Y TECNOL&Oacute;GICOS DELESTADO DE M&Eacute;XICO.</font>
     <hr width="100%">
@@ -18,15 +17,17 @@
         <h1>PLANTEL VALLE DE BRAVO</h1>
         <h2>- REGISTRAR ESTUDIANTE - </h2>  
         <input type="text" name="N_CONTROL" placeholder="Número de control">
-        <input type="text" name=" N_ESTUDIANTE" placeholder="Nombre estudiante">
-        <input type="text" name=" AP_PATERNO" placeholder="Apellido Paterno">
-        <input type="text" name="AP_MATERNO" placeholder="Apellido Materno">
+        <input type="text" name="N_ESTUDIANTE" placeholder="Nombre estudiante">
+        <input type="text" name="AP_PATERNO" placeholder="Apeido Paterno">
+        <input type="text" name="AP_MATERNO" placeholder="Apeido Materno">
         <input type="email" name="CORREO_INS" placeholder="Correo institucional">
-        <input type="submit" name="lista_estudiantes">
+        <input type="submit" name="listaestudiantes">
+
     </form>
     
     <?php
         include("registro.php");
-  ?>
+     ?>
 </body>
 </html>
+
